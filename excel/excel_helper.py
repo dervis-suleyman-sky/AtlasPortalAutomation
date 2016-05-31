fae8f81c8a052599c2ad573dc41fa0c35cfb3564
dervis.s <dervis.suleyman@sky.uk>
2016-05-31 13:14:47 +0100

    Added Excel Helper file begun automating the excel sheet

---
+++ excel/excel_helper.py
@@ -0,0 +1,19 @@
+'''
+Created on 31 May 2016
+
+@author: Dev2
+'''
+
+from openpyxl import load_workbook
+
+class ExcelHelper(object):
+
+
+    def __init__(self):
+        pass
+    
+'''
+Test Spread sheet reading code
+'''
+if __name__ == '__main__':
+    print("hello world")
