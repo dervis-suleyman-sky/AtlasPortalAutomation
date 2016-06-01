159ec34c72fe8ef8f7731934d3a86f2daa0c9658
dervis.s <dervis.suleyman@sky.uk>
2016-06-01 14:15:42 +0100

    Added code to read from a excel spread sheet
    Added code to execute from a spread sheet

--- excel/excel_helper.py
+++ excel/excel_helper.py
@@ -8,48 +8,229 @@ from openpyxl import load_workbook
 import warnings
 
 class ExcelHelper(object):
+    '''
+    Test Spread sheet reading code
+    
+    
+    What I want to do is open the excel spread sheet check there is a value in Operation if not, stop the code
+    
+    read row=3
+    read col=range[A:BJ]
+    
+    when test is complete:
+        
+        ##create an asset via the portal## mark as pass or fail and move onto the next row. break if row contains no operation
+    
+    read next row
+    
+    '''
 
+    def __init__(self,excel_spreadsheet='C:\\workspace\\AtlasPortalAutomation\\excel\\excel_documents\\test.xlsm'):
+        print('Reading Test Data from:'+excel_spreadsheet)
+        self.excel_location=excel_spreadsheet
+    
+    '''
+    @author: Dervis Suleyman
+    @summary: reads a sheet from an excel spread sheet and returns the cell value
+    '''
+    def get_cell_value(self,sheet=None,row_number=None,col_number=None):
+        cell_value=(sheet.cell(row=row_number, column=col_number).value)
+        return cell_value
+    
+    '''
+    @author: Derivs Suleyman
+    @note: Handle the test set creation from spread sheet - need to identify if it's an update or delete test case
+    '''
+    def get_test_set(self):
+        '''store each asset row as a test inside the test set'''
+        test_set={"test_set" : {}}
+        
+        '''Ignore the warning for using .xlsm this is due to the python code expecting a 2006 extension'''
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            '''--------------------------------------------------------------------------------------------'''
+            wb = load_workbook(filename=self.excel_location)
+            print("-----------------------------------------")
+            print("Active work sheet=["+str(wb.active)+"]")
+            #read the Meta data work sheet
+            sheet=wb.get_sheet_by_name('Metadata spreadsheet')
+            #check the title of the work sheet can be found
+            #print(sheet.title)
+            print("-----------------------------------------")
+            
+            
+            '''iterate over each row in the excel spread sheet need to handle utf-8 encoding'''
+            for row_number in range(4, 100):
+                '''stop the loop if there is no value in the operation field'''
+                operation=self.get_cell_value(sheet, row_number, col_number=1)
+                                              
+                if (operation==None or len(operation)<=0):
+                    print("EOF...stop reading the spread sheet")
+                    print("-----------------------------------------")
+                    break;
+                
+                '''Test Place Holder'''
+                test='test'+str(row_number)
+                '''update test set with new information'''
+                test_set['test_set'].update({test:{}})
+                
+                '''Populate the series info based in information from the spread sheet'''
+                series_info = {'SeriesID':self.get_cell_value(sheet, row_number, col_number=11),
+                       'TitleBrief':self.get_cell_value(sheet, row_number, col_number=31),
+                       'TitleMedium':self.get_cell_value(sheet, row_number, col_number=32),
+                       'TitleLong':self.get_cell_value(sheet, row_number, col_number=33),
+                       'SummaryBrief':self.get_cell_value(sheet, row_number, col_number=34),
+                       'SummaryShort':self.get_cell_value(sheet, row_number, col_number=35),
+                       'SummaryMedium':self.get_cell_value(sheet, row_number, col_number=36),
+                       'SummaryLong':self.get_cell_value(sheet, row_number, col_number=37),
+                       'Genre':self.get_cell_value(sheet, row_number, col_number=14),
+                       'StudioDisplay':self.get_cell_value(sheet, row_number, col_number=15),
+                       '16x9 image':self.get_cell_value(sheet, row_number, col_number=55),
+                       '4x3 image':self.get_cell_value(sheet, row_number, col_number=57)}
+                
+                test_set['test_set'][test].update({'series_info':series_info})
+                
+                season_info = {'SeasonID':self.get_cell_value(sheet, row_number, col_number=12),
+                       'Series':self.get_cell_value(sheet, row_number, col_number=32),
+                       'TitleBrief':self.get_cell_value(sheet, row_number, col_number=38),
+                       'TitleMedium':self.get_cell_value(sheet, row_number, col_number=39),
+                       'TitleLong':self.get_cell_value(sheet, row_number, col_number=40),
+                       'SummaryBrief':self.get_cell_value(sheet, row_number, col_number=41),
+                       'SummaryShort':self.get_cell_value(sheet, row_number, col_number=42),
+                       'SummaryMedium':self.get_cell_value(sheet, row_number, col_number=43),
+                       'SummaryLong':self.get_cell_value(sheet, row_number, col_number=44),
+                       'SeasonNumber':self.get_cell_value(sheet, row_number, col_number=20),
+                       'TotalEpisodes':self.get_cell_value(sheet, row_number, col_number=21),
+                       'ProductionYear':str(self.get_cell_value(sheet, row_number, col_number=13)),
+                       '16x9 image':self.get_cell_value(sheet, row_number, col_number=56),
+                       '4x3 image':self.get_cell_value(sheet, row_number, col_number=58)}
+                
+                test_set['test_set'][test].update({'season_info':season_info})
+                
+                portal_asset={'asset_id':self.get_cell_value(sheet, row_number, col_number=2),
+                      'channel':self.get_cell_value(sheet, row_number, col_number=3),
+                      'series':self.get_cell_value(sheet, row_number, col_number=32),
+                      'season':self.get_cell_value(sheet, row_number, col_number=38),
+                      'episodeNumber':self.get_cell_value(sheet, row_number, col_number=19),
+                      'TotalEpisodes':self.get_cell_value(sheet, row_number, col_number=20),
+                      'Title':self.get_cell_value(sheet, row_number, col_number=24),
+                      'Summary':self.get_cell_value(sheet, row_number, col_number=30),
+                      'Actors':self.get_cell_value(sheet, row_number, col_number=45),
+                      'Warning':self.get_cell_value(sheet, row_number, col_number=17),
+                      'DisplayRuntime':str(self.get_cell_value(sheet, row_number, col_number=18)),#may cause problems
+                      'Genre':self.get_cell_value(sheet, row_number, col_number=14),
+                      'Rating':self.get_cell_value(sheet, row_number, col_number=16),
+                      'BroadcastDate':str(self.get_cell_value(sheet, row_number, col_number=5)),
+                      'ProductionYear':str(self.get_cell_value(sheet, row_number, col_number=13)),
+                      'Studio':self.get_cell_value(sheet, row_number, col_number=15),
+                      'VideoFile':self.get_cell_value(sheet, row_number, col_number=47),
+                      'SD/HD':self.get_cell_value(sheet, row_number, col_number=48),
+                      'SOE':str(self.get_cell_value(sheet, row_number, col_number=49)),
+                      'SOM':str(self.get_cell_value(sheet, row_number, col_number=50)),
+                      'EOM':str(self.get_cell_value(sheet, row_number, col_number=51)),
+                      'SOC':str(self.get_cell_value(sheet, row_number, col_number=52)),
+                      'Duration':str(self.get_cell_value(sheet, row_number, col_number=53)),
+                      'AspectRatio':self.get_cell_value(sheet, row_number, col_number=54),
+                      'SubTitles':self.get_cell_value(sheet, row_number, col_number=56),
+                      '16-9-image':self.get_cell_value(sheet, row_number, col_number=60),
+                      '4-3-image':self.get_cell_value(sheet, row_number, col_number=62),
+                      'Boxart-image':self.get_cell_value(sheet, row_number, col_number=63)}
+                
+                test_set['test_set'][test].update({'portal_asset':portal_asset})
+                
+                
+                '''
+                If offer from the spread sheet contains AT or AM or AS
+                platform = "AT AM AS"
+                platform = "AM AS"
+                platform = "AT"
+                '''
+                
+                platform = self.get_cell_value(sheet, row_number, col_number=7)
+                
+                if 'AT' in platform:
+                    
+                    offer={'Platform':'AT',
+                    'Type':self.get_cell_value(sheet, row_number, col_number=8),
+                    'StartDate':str(self.get_cell_value(sheet, row_number, col_number=5)),
+                    'EndDate':str(self.get_cell_value(sheet, row_number, col_number=6))
+                    }
+                    
+                    '''check for the platform key if it does not exist create it'''
+                    if test_set['test_set'][test].has_key('platform')==False:
+                        test_set['test_set'][test].update({'platform':{}})
+                    
+                    test_set['test_set'][test]['platform'].update({'AT':offer})
+ 
+                
+                if 'AM' in platform:
+                    
+                    offer={'Platform':'AM',
+                    'Type':self.get_cell_value(sheet, row_number, col_number=8),
+                    'StartDate':str(self.get_cell_value(sheet, row_number, col_number=5)),
+                    'EndDate':str(self.get_cell_value(sheet, row_number, col_number=6))
+                    }
+                    
+                    '''check for the platform key if it does not exist create it'''
+                    if test_set['test_set'][test].has_key('platform')==False:
+                        test_set['test_set'][test].update({'platform':{}})
+                    
+                    test_set['test_set'][test]['platform']['AM']=offer
+                    
+                if 'AS' in platform:
+                    
+                    offer={'Platform':'AS',
+                    'Type':self.get_cell_value(sheet, row_number, col_number=8),
+                    'StartDate':str(self.get_cell_value(sheet, row_number, col_number=5)),
+                    'EndDate':str(self.get_cell_value(sheet, row_number, col_number=6))
+                    }
+                    
+                    '''check for the platform key if it does not exist create it'''
+                    if test_set['test_set'][test].has_key('platform')==False:
+                        test_set['test_set'][test].update({'platform':{}})
+                    
+                    test_set['test_set'][test]['platform']['AS']=offer
 
-    def __init__(self):
+        return test_set
+    
+    '''to be completed'''
+    def update(self):
         pass
     
-'''
-Test Spread sheet reading code
-
-
-What I want to do is open the excel spread sheet check there is a value in Operation if not, stop the code
-
-read row=3
-read col=range[A:BJ]
-
-when test is complete:
+    '''to be completed'''
+    def delete(self):
+        pass
     
-    ##create an asset via the portal## mark as pass or fail and move onto the next row. break if row contains no operation
 
-read next row
-
-'''
 if __name__ == '__main__':
-    with warnings.catch_warnings():
-        '''Ignore the warning for using .xlsm this is due to the python code expecting a 2006 extension'''
-        warnings.simplefilter("ignore")
-        '''--------------------------------------------------------------------------------------------'''
-        wb = load_workbook(filename='C:\\workspace\\AtlasPortalAutomation\\excel\\excel_documents\\NBCU_Syfy_Metadata Schedule_160524_RN.xlsm')
-        print("-----------------------------------------")
-        print("Active work sheet=["+str(wb.active)+"]")
-        #read the Metadata work sheet
-        sheet=wb.get_sheet_by_name('Metadata spreadsheet')
-        #check the title of the work sheet can be found
-        print(sheet.title)
-        print("-----------------------------------------")
-        
-        '''iterate over each row'''
-        for row_number in range(4, 8):
-            for col_number in range(1,63):
-                cell_value=(sheet.cell(row=row_number, column=col_number).value)
-                if not cell_value==None:
-                    print cell_value
+    excel_helper = ExcelHelper('C:\\workspace\\AtlasPortalAutomation\\excel\\excel_documents\\test.xlsm')
+    test_set = excel_helper.get_test_set()
+    print(type(test_set))
+    #print test_set['test_set']['test5']['series_info']['SummaryMedium']
+    import json
+    print json.dumps(test_set,indent=4, separators=(',', ': ')) 
 
-        
-        #print sheet['A4'].value,sheet['B4'].value,sheet['C4'].value,sheet['AA4'].value
-    
+'''
+@note: for testing
+'''
+# if __name__ == '__main__':
+#     with warnings.catch_warnings():
+#         '''Ignore the warning for using .xlsm this is due to the python code expecting a 2006 extension'''
+#         warnings.simplefilter("ignore")
+#         '''--------------------------------------------------------------------------------------------'''
+#         wb = load_workbook(filename='C:\\workspace\\AtlasPortalAutomation\\excel\\excel_documents\\test.xlsm')
+#         print("-----------------------------------------")
+#         print("Active work sheet=["+str(wb.active)+"]")
+#         #read the Metadata work sheet
+#         sheet=wb.get_sheet_by_name('Metadata spreadsheet')
+#         #check the title of the work sheet can be found
+#         print(sheet.title)
+#         print("-----------------------------------------")
+#         
+#         '''iterate over each row'''
+#         for row_number in range(4, 8):
+#             for col_number in range(1,63):
+#                 cell_value=(sheet.cell(row=row_number, column=col_number).value)
+#                 if not cell_value==None:
+#                     print cell_value
+                    
---
+++ isilon/logging_example.py
@@ -0,0 +1,16 @@
+'''
+Created on 19 May 2016
+
+@author: Dev2
+'''
+import time
+import logging
+
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s:%(asctime)s %(message)s', level=logging.DEBUG)
+    while True:
+            logging.info('moving ['+'dads'+'] into ['+'sdadsa'+']')
+            time.sleep(0.5)
